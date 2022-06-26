318bff121a819c844c6ac0f8b533cfef36e19402
METenedini <107526114+METenedini@users.noreply.github.com>
2022-06-26 19:13:42 -0300

    Update menuCRUD.cpp

--- menuCRUD.cpp
+++ menuCRUD.cpp
@@ -6,19 +6,16 @@ void controle_estoque(FILE *Arquivo,FILE * Arquivo1){
 	
 	while(fread(&est,sizeof(struct produto),1,Arquivo1) and fread(&novo,sizeof(struct compra_venda),1,Arquivo) ){
 		
-		rewind(Arquivo);
-		rewind(Arquivo1);		
-			 int qnt_est = est.qtd;
-	 int qnt_des =novo.qtd;
-	qnt_est = qnt_est - qnt_des;
-	printf("%d",qnt_est);	
 		
-	if(qnt_est >= 0){
+
+	est.qtd=est.qtd-novo.qtd
+		printf("\n%d",est.qtd);
+	if(est.qtd >= 0){
 		rewind(Arquivo1);
 	while (fread (&est, sizeof(est), 1, Arquivo1))
 	if(novo.codigoprod==est.cdgprod){
 		 fseek(Arquivo1,sizeof(struct produto)*-1, SEEK_CUR);
-		est.qtd=est.qtd- qnt_des;   
+		est.qtd=est.qtd- novo.qtd  
 		fwrite (&est, sizeof(est), 1, Arquivo1);
 	fseek(Arquivo1, sizeof(est)* 0, SEEK_END);
 	}
@@ -38,3 +35,4 @@ void controle_estoque(FILE *Arquivo,FILE * Arquivo1){
 		} 
 	}
 }
+
